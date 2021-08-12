ef222b7aa97d6b3da5555d72b394fada595fb894
mamark90 <mamark90@gmail.com>
2021-08-12 12:01:33 -0400

    commit tree c6149d78d664b10b7ae05668f6571669228b4ef3

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+  const firstTwoDrivers = drivers.slice(0,2);
+  return firstTwoDrivers;
+  }
+
+const returnLastTwoDrivers = function(drivers) {
+  const lastTwoDrivers = drivers.slice(-2);
+  return lastTwoDrivers;
+  }
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(multiplier) {
+  const calculatedFare = function(fare){
+  return (multiplier * fare);
+  } 
+  return calculatedFare;
+}
+
+const fareDoubler = function(fare) {
+  const doubleFare = createFareMultiplier(2)(fare);
+  return doubleFare;
+}
+
+const fareTripler = function(fare) {
+  const tripleFare = createFareMultiplier(3)(fare);
+  return tripleFare;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, firstTwoDrivers, lastTwoDrivers) {
+  if (firstTwoDrivers) {
+    return firstTwoDrivers(arrayOfDrivers) 
+  } else if (lastTwoDrivers) {
+    return lastTwoDrivers(arrayOfDrivers)
+  } else {
+    console.log("Invalid function")
+  }
+}
+
